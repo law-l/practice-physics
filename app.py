723b07a275150b15b5459eda7fd5dac247d4bbe0
Lawrence Lai <law@Lawrences-MacBook-Pro.local>
2024-11-17 13:38:20 -0500

    test in dev

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-from util.model import Problem
-
 import streamlit as st
-import os
-import pandas as pd
 
-
-@st.cache_data()
-def load_problems(sheet_url: str) -> pd.DataFrame:
-    """Load all problems from Google Sheet"""
-    csv_export_url = sheet_url.replace('/edit?gid=', '/export?format=csv&gid=')
-    df = pd.read_csv(csv_export_url).astype(str)
-    return df
-
-
-def sample_problem(df: pd.DataFrame) -> dict:
-    """Randomly pick a problem"""
-    return df.sample(1).to_dict(orient="records")[0]
-
-
-df_problems = load_problems(st.secrets["GSHEET_PROBLEMS"])
+from util.db import log_submission, sample_problem
+from util.model import Problem
 
 # initialize session_state
 if "is_reloaded" not in st.session_state or st.session_state["is_reloaded"]:
@@ -28,46 +11,65 @@ if "is_reloaded" not in st.session_state or st.session_state["is_reloaded"]:
     st.session_state["is_solution_shown"] = False
     st.session_state["response"] = None
     st.session_state["is_reloaded"] = False
-    st.session_state["problem"] = sample_problem(df_problems)
+    st.session_state["problem"] = sample_problem()
 
-    # # get data
-    # sheet_url = os.environ["GSHEET_PROBLEM"]
-    # csv_export_url = sheet_url.replace('/edit?gid=', '/export?format=csv&gid=')
-    # st.session_state["problem"] = pd.read_csv(csv_export_url).astype(str).sample(1).to_dict(orient="records")[0]
-
-# reload button
-if st.button("Reload"):
-    st.session_state["is_submitted"] = False
-    st.session_state["is_reloaded"] = True
-    st.rerun()
-
+# make sure problem is in the correct format
 problem = Problem.model_validate(st.session_state["problem"])
+
+# display question
 st.subheader("Question")
 st.write(f"[ID: {problem.id}] {problem.question}")
 
+# display options
 response = st.radio(
-    label="Select your answer and click submit", 
+    label="Select your answer and click submit",
     options=[problem.option_1, problem.option_2, problem.option_3, problem.option_4],
     disabled=st.session_state["is_submitted"],
 )
+
+# if the submit button has not been clicked, keep track of user repsonse.
+# once submit button has been clicked, stop updating user response.
 if not st.session_state["is_submitted"]:
     st.session_state["response"] = response
 
-is_submitted = st.button(
-    label="Submit",
-    disabled=st.session_state["is_submitted"],
-)
+col1, col2, col3 = st.columns(3)
+
+# display submit button
+with col1:
+    is_submitted = st.button(
+        label="Submit",
+        disabled=st.session_state["is_submitted"],
+    )
+with col3:
+    # reload button
+    if st.button("Reload"):
+        st.session_state["is_submitted"] = False
+        st.session_state["is_reloaded"] = True
+        st.rerun()
+
+# if the submit button has been clicked the first time, log submission
+if is_submitted and not st.session_state["is_submitted"]:
+    log_submission(
+        user="test_user",
+        problem_id=problem.id,
+        correct=st.session_state["response"] == problem.answer,
+    )
+
+# if the submit button has ever been clicked, then problem is considered submitted.
 if is_submitted or st.session_state["is_submitted"]:
     st.session_state["is_submitted"] = True
 
+# if the submit button has ever been clicked, check answers
 if st.session_state["is_submitted"]:
     if st.session_state["response"] == problem.answer:
         st.success(f"That is correct.")
     else:
         st.error(f"That is incorrect.")
 
+# if the submit button has ever been clicked, display show solution button
 if st.session_state["is_submitted"]:
-    is_solution_shown = st.button(label="Show solution")
+    with col2:
+        is_solution_shown = st.button(label="Show solution")
     if is_solution_shown or st.session_state["is_solution_shown"]:
         st.session_state["is_solution_shown"] = True
         st.subheader("Solution")
