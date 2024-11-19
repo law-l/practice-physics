deb0261394c7caed3b1d413aa27e0ab9eabe6fb6
Lawrence Lai <law@Lawrences-MacBook-Pro.local>
2024-11-18 23:54:58 -0500

    update home

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Home", page_icon="🏠")
 content = """
 # Welcome to Physics Regents Practice! 👋
 
-Welcome to **Physics Regents Practice** – your ultimate tool for mastering the New York State Physics Regents Exam! Whether you're aiming for a top score or just need some extra practice, you've come to the right place. 
+This app is your ultimate tool for mastering the New York State Physics Regents Exam! Whether you're aiming for a top score or just need some extra practice, you've come to the right place. 
 
 ## What We Offer
 - ✍🏼 **Hundreds of problems:** Practice with plenty of Physics Regents questions. You’ll be prepared for whatever comes your way on exam day!
@@ -14,7 +14,7 @@ Welcome to **Physics Regents Practice** – your ultimate tool for mastering the
 
 ## Get Started Now
 1. 🧠 **Practice** – Head to the Practice page on the side bar. Sign in with your email address and start practicing questions from a topic of your choice.
-3. 🚀 **Leaderboard** – Answer questions, earn points, and watch your rank rise on the Leaderboard page!
+2. 🚀 **Leaderboard** – Answer questions, earn points, and watch your rank rise on the Leaderboard page!
 
 No matter where you are in your studies, this app will help you build your skills and confidence to achieve your best score on the Physics Regents. So, what are you waiting for? Let’s get practicing!
 """
